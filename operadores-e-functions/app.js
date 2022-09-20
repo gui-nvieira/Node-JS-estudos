7f202e783039d28e372e2bd771295e06ab4e047d
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2022-09-20 16:56:52 -0300

    adicionados comentarios

--- operadores-e-functions/app.js
+++ operadores-e-functions/app.js
@@ -1,8 +1,10 @@
+// chamada da funcao para inserir valores via terminal  - > readLine
 const readLine = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
+// nesse caso eh possivel verificar se o valor inserido eh um numero a partir do parse dele, se retornar null-> false
+// e o if vai servir como tratamento de erro
 const validarNum = (numero) => {
         const resultado = Number.parseFloat(numero)  
         if (!resultado){
@@ -10,7 +12,7 @@ const validarNum = (numero) => {
         }
         return resultado
 }
-
+// esse case servira para validar o operador, eh possível usar cases suscessivos com um só return
 const validarOp = (operador) =>{
     switch(operador){
         case '+':
@@ -25,6 +27,7 @@ const validarOp = (operador) =>{
     }
 }
 
+// o comportamento principal vai entregar valores que passaram pelos validadores e executar a operacao matematica
 readLine.question('Favor informar um número: ', (numero1)=>{
     const numeroValidado1 = validarNum(numero1);
     if(numeroValidado1){
